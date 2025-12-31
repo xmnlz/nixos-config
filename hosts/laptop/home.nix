f014221018f6349266840141b6922e0e7bb341a9
xmnlz <lemmeq9@gmail.com>
2025-12-31 15:09:35 +0100

    another try

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -3,23 +3,14 @@
 {
   home.stateVersion = "25.11";
 
-  home.username = "xmnlz";
-  home.homeDirectory = "/home/xmnlz";
-
-  xdg.configFile."hypr".source = ./hypr;
-
-  wayland.windowManager.hyprland.enable = true;
-
   home.packages = with pkgs; [
-    stable.git
-    neovim
     google-chrome
     telegram-desktop
     gh
   ];
 
-  programs.git = {
-    enable = true;
-  };
+  imports = [
+    ../../modules/home/common.nix
+  ];
 
 }
