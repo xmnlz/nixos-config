7f6478956ed042c79ef37cdeb35cbad0ea1a30ef
= <=>
2026-01-16 14:49:22 +0100

    fix 2

--- flake.nix
+++ flake.nix
@@ -9,26 +9,19 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-    };
-
-    vicinae = {
-      url = "github:vicinaehq/vicinae";
-    };
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    vicinae.url = "github:vicinaehq/vicinae";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-      nixosConfigurations.laptop = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-
-        modules = [
-          inputs.home-manager.nixosModules.home-manager
-          ./hosts/laptop
-        ];
-        specialArgs = { inherit inputs; };
-      };
-    };
+    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        home-manager.nixosModules.home-manager
+        ./hosts/laptop
+      ];
+    }; 
 
     homeConfigurations."xmnlz" = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
@@ -37,5 +30,6 @@
         ./hosts/laptop/home.nix 
         inputs.vicinae.homeManagerModules.default 
       ];
-  };
+    };
+  }; 
 }
