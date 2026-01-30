4f0cb660372849c34d7883552332572cca3ff43d
= <=>
2026-01-30 13:59:39 +0100

    feat: nvim

--- modules/home/neovim/nvim/lua/config/lsp.lua
+++ modules/home/neovim/nvim/lua/config/lsp.lua
@@ -15,7 +15,6 @@ vim.lsp.enable {
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     local bufnr = args.buf
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
     local opts = { noremap = true, silent = true, buffer = bufnr }
 
     -- Define keybindings
