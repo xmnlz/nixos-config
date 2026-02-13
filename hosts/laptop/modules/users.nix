924c0e94a2b803b577e76f42d516dd0d3db17609
= <=>
2026-02-13 23:50:58 +0100

    fix the dconf

--- hosts/laptop/modules/users.nix
+++ hosts/laptop/modules/users.nix
@@ -7,6 +7,26 @@
     extraGroups = [ "networkmanager" "wheel" ];
   };
 
+  dconf.settings = {
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      # Disable GNOME’s default Super+Space (input source switcher)
+      switch-input-source = [];
+      switch-input-source-backward = [];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/vicinae/"
+      ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/vicinae" = {
+      name = "Vicinae";
+      command = "vicinae";
+      binding = "<Super>space";
+    };
+  };
+
   users.users.greeter = {
     isSystemUser = true;
     home = "/var/lib/greeter";
