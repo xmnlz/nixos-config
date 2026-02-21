741b6f8eef0aa0d2d58dd8180138d78bad3acfcb
xmnlz <lemmeq9@gmail.com>
2026-02-21 19:43:21 +0100

    FUCK SYSTEMD

--- home/apps/vicinae.nix
+++ home/apps/vicinae.nix
@@ -7,7 +7,11 @@
     systemd = {
       enable = true;
       autoStart = true;
+      environment = {
+        USE_LAYER_SHELL = 1;
+      };
     };
+
 
     extensions = with inputs.vicinae-extensions.packages.${pkgs.stdenv.hostPlatform.system}; [
       nix
