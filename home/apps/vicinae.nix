0818e04482d598d4fbf40221aed6bdabfc00bf2a
xmnlz <lemmeq9@gmail.com>
2026-02-21 18:57:27 +0100

    VICINAE GO

--- home/apps/vicinae.nix
+++ home/apps/vicinae.nix
@@ -3,7 +3,12 @@
 {
   services.vicinae = {
     enable = true;
-    systemd.enable = true;
+
+    systemd = {
+      enable = true;
+      autoStart = true;
+    };
+
     extensions = with inputs.vicinae-extensions.packages.${pkgs.stdenv.hostPlatform.system}; [
       nix
       power-profile
