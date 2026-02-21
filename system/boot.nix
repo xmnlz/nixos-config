57d2e7fa57b57895331dc2dfad031dbb81cb8fc0
xmnlz <lemmeq9@gmail.com>
2026-02-22 00:47:19 +0100

    FIX WORK

--- system/boot.nix
+++ system/boot.nix
@@ -2,6 +2,14 @@
 
 {
   boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelParams = [
+    "amdgpu.dc=1"
+    "amdgpu.runpm=0"
+    "amdgpu.aspm=0"
+    "amdgpu.dpm=1"
+    "video=efifb:off"
+    "ucsi_acpi.disable_usb_errors=1"
+  ];
 
   boot.loader = {
     limine = {
