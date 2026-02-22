afcdeeed5325e905dc419a2dc756f4f0a32b4ec7
xmnlz <lemmeq9@gmail.com>
2026-02-22 12:09:21 +0100

    FUCK all of this shit

--- system/gnome.nix
+++ system/gnome.nix
@@ -4,6 +4,15 @@
   programs.hyprland = {
     enable = true;
     withUWSM = true;
+    xwayland.enable = true;
+  };
+
+  programs.uwsm.waylandCompositors = {
+    hyprland = {
+      prettyName = "Hyprland";
+      comment = "Hyprland compositor managed by UWSM";
+      binPath = "/run/current-system/sw/bin/start-hyprland";
+   };
   };
 
   services.xserver.enable = true;
