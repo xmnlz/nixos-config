1cdd7cfc899550004fcf6e94f69f3434b8593def
xmnlz <lemmeq9@gmail.com>
2026-02-25 00:27:08 +0100

    fix the qs issues, im dum

--- home/gaming.nix
+++ home/gaming.nix
@@ -2,10 +2,7 @@
 
 {
   home.sessionVariables = {
-    XDG_SESSION_TYPE = "wayland";
-    WAYLAND_DISPLAY = "wayland-0";
-    SDL_VIDEODRIVER = "wayland";
-    QT_QPA_PLATFORM = "wayland";
     NIXOS_OZONE_WL = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "wayland";
   };
 }
