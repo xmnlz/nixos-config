237fd88a8419641158d98fb147ee01447c06e0b7
xmnlz <lemmeq9@gmail.com>
2026-02-28 10:41:03 +0100

    fish: updata promt

--- home/development.nix
+++ home/development.nix
@@ -22,6 +22,32 @@
   programs.fish = {
     enable = true;
 
+    functions.fish_prompt = ''
+      set -l last_status $status
+
+      set -l dir_color (set_color cyan)
+      set -l git_color (set_color magenta)
+      set -l error_color (set_color red)
+      set -l normal (set_color normal)
+
+      set -l cwd (prompt_pwd)
+
+      set -l git_branch
+      if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+          set git_branch (command git branch --show-current 2>/dev/null)
+      end
+
+      set -l stat
+      if test $last_status -ne 0
+          set stat "$error_color[$last_status]$normal "
+      end
+
+      printf "%s%s%s%s ❯ " \
+        "$stat" \
+        "$dir_color$cwd$normal" \
+        (test -n "$git_branch"; and printf " $git_color%s$normal" "$git_branch") \
+        ""
+    '';
   };
 
   programs.git = {
