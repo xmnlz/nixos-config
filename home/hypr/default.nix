35aae4007df505b481a1719ce691372d6e0b45f5
xmnlz <lemmeq9@gmail.com>
2026-03-03 11:35:06 +0100

    should work

--- home/hypr/default.nix
+++ home/hypr/default.nix
@@ -2,4 +2,10 @@
 
 {
   xdg.configFile."hypr/hyprland.conf".source = ./hyprland.conf;
+  xdg.configFile."hypr/xdph.conf".text = '' 
+  screencopy {
+    allow_token_by_default = true
+    custom_picker_binary = hyprland-preview-share-picker
+  }
+  ''
 }
