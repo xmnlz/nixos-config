30587e036339debb83a54041339586e2af4f4f1e
xmnlz <lemmeq9@gmail.com>
2026-03-20 22:40:07 +0100

    fix: no more bracking grimblast

--- home/hypr/default.nix
+++ home/hypr/default.nix
@@ -19,4 +19,9 @@
   };
 
   xdg.configFile."hypr/hyprland.conf".source = ./hyprland.conf;
+  xdg.configFile."hypr/xdph.conf".text = '' 
+  screencopy {
+    allow_token_by_default = true
+  }
+  '';
 }
