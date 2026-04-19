c3cbcac05ff0ab01c72dfeac97d7f7298765ad26
xmnlz <lemmeq9@gmail.com>
2026-04-19 22:50:57 +0200

    gui: added dms

--- flake.nix
+++ flake.nix
@@ -11,6 +11,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    dms = {
+      url = "github:AvengeMedia/DankMaterialShell/stable";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     hyprland-contrib = {
       url = "github:hyprwm/contrib";
       inputs.nixpkgs.follows = "nixpkgs";
