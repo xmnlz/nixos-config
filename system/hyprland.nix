b14c19a5f6facc73767cd24af69ada8c716fbf4e
xmnlz <lemmeq9@gmail.com>
2026-04-21 18:50:53 +0200

    fix: they fixed the gnome issue

--- system/hyprland.nix
+++ system/hyprland.nix
@@ -4,7 +4,6 @@
   programs.hyprland = {
     enable = true;
     withUWSM = true;
-    xwayland.enable = true;
   };
 
   xdg.portal = {
@@ -14,13 +13,4 @@
   };
 
   services.displayManager.gdm.enable = true;
-
-  # FIXME: https://github.com/NixOS/nixpkgs/issues/484328
-  programs.uwsm.waylandCompositors = {
-    hyprland = {
-      prettyName = "Hyprland";
-      comment = "Hyprland compositor managed by UWSM";
-      binPath = "/run/current-system/sw/bin/start-hyprland";
-   };
-  };
 }
