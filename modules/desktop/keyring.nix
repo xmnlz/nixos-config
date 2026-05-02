2ef45f0efaa84d00316d08fa1102c0d2b181806a
xmnlz <lemmeq9@gmail.com>
2026-05-02 15:18:26 +0200

    feat: add keyring

---
+++ modules/desktop/keyring.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }: 
+
+{
+  services.gnome.gnome-keyring.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    gnome-keyring
+    libsecret
+    seahorse
+  ];
+
+  security.pam.services.hyprlock.enableGnomeKeyring = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
+
+}
