d8fe1d314ab99b396cee226b81c7f8a05a59ed27
xmnlz <lemmeq9@gmail.com>
2026-05-06 01:13:37 +0200

    fix: alacritty added paddings

--- home/gui/alacritty.nix
+++ home/gui/alacritty.nix
@@ -4,7 +4,14 @@
   programs.alacritty = {
     enable = true;
     settings = {
-      window.opacity = 0.95;
+      window = {
+        opacity = 0.95;
+        dynamic_padding = true;
+        padding = {
+          x = 4;
+          y = 4;
+        };
+      };
     };
   };
 }
