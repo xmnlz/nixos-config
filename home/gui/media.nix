339d5eeef8238e69f89249282d91b26e0d1370aa
xmnlz <lemmeq9@gmail.com>
2026-05-24 14:07:24 +0200

    feat: add mvp player

--- home/gui/media.nix
+++ home/gui/media.nix
@@ -9,4 +9,21 @@
   programs.obs-studio = {
     enable = true;
   };
+
+  programs.mpv.enable = true;
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "video/mp4"             = [ "mpv.desktop" ];
+      "video/x-matroska"      = [ "mpv.desktop" ];
+      "video/webm"            = [ "mpv.desktop" ];
+      "video/avi"             = [ "mpv.desktop" ];
+      "video/quicktime"       = [ "mpv.desktop" ];
+      "video/x-msvideo"       = [ "mpv.desktop" ];
+      "video/mpeg"            = [ "mpv.desktop" ];
+      "video/ogg"             = [ "mpv.desktop" ];
+      "video/3gpp"            = [ "mpv.desktop" ];
+      "application/x-mpegURL" = [ "mpv.desktop" ];
+    };
+  };
 }
