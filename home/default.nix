5d3de8039b3ce99fa834c7bde09995ce9efdf153
xmnlz <lemmeq9@gmail.com>
2026-05-28 10:16:43 +0200

    feat: add lazygit

--- home/default.nix
+++ home/default.nix
@@ -1,38 +1,39 @@
-{ inputs, ... }:
+{inputs, ...}: {
+  imports =
+    [
+      ./shell.nix
+      ./languages.nix
 
-{
-  imports = [
-    ./shell.nix
-    ./languages.nix
+      ./desktop/hyprland
+      ./desktop/dms.nix
+      ./desktop/qt.nix
+      ./desktop/gtk.nix
 
-    ./desktop/hyprland
-    ./desktop/dms.nix
-    ./desktop/qt.nix
-    ./desktop/gtk.nix
+      ./gui/zen.nix
+      ./gui/chromium.nix
 
-    ./gui/zen.nix
-    ./gui/chromium.nix
+      ./gui/communication.nix
+      ./gui/alacritty.nix
+      ./gui/vicinae.nix
+      ./gui/files.nix
+      ./gui/gaming.nix
+      ./gui/media.nix
+      ./gui/notes.nix
 
-    ./gui/communication.nix
-    ./gui/alacritty.nix
-    ./gui/vicinae.nix
-    ./gui/files.nix
-    ./gui/gaming.nix
-    ./gui/media.nix
-    ./gui/notes.nix
+      ./cli/git.nix
+      ./cli/direnv.nix
+      ./cli/fastfetch.nix
+      ./cli/utils.nix
 
-    ./cli/git.nix
-    ./cli/direnv.nix
-    ./cli/btop.nix
-    ./cli/fastfetch.nix
-    ./cli/utils.nix
-
-    ./tui/opencode.nix
-    ./tui/tmux
-    ./tui/neovim
-  ] ++ [
-    inputs.vicinae.homeManagerModules.default
-    inputs.dms.homeModules.dank-material-shell
-    inputs.zen-browser.homeModules.beta
-  ];
+      ./tui/opencode.nix
+      ./tui/lazygit.nix
+      ./tui/btop.nix
+      ./tui/tmux
+      ./tui/neovim
+    ]
+    ++ [
+      inputs.vicinae.homeManagerModules.default
+      inputs.dms.homeModules.dank-material-shell
+      inputs.zen-browser.homeModules.beta
+    ];
 }
