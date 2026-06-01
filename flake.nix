304faa6445e5b03f900b5c8eb042f69360d289dc
xmnlz <lemmeq9@gmail.com>
2026-06-01 11:20:27 +0200

    feat: swicth to new channel

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
