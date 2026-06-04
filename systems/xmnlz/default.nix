e5582bcecb9d029b659b6d48183f6db59554ec4d
xmnlz <lemmeq9@gmail.com>
2026-06-04 11:23:02 +0200

    fix: remove nixld

--- systems/xmnlz/default.nix
+++ systems/xmnlz/default.nix
@@ -33,15 +33,15 @@
     shell = pkgs.fish;
   };
 
-  # TODO: Fix this
-  programs.nix-ld = {
-    enable = true;
-    libraries = with pkgs; [
-      stdenv.cc.cc.lib
-      zlib
-      openssl
-    ];
-  };
+  # # TODO: Fix this
+  # programs.nix-ld = {
+  #   enable = true;
+  #   libraries = with pkgs; [
+  #     stdenv.cc.cc.lib
+  #     zlib
+  #     openssl
+  #   ];
+  # };
 
   home-manager = {
     verbose = true;
