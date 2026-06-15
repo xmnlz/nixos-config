9e1d2938860a8f7a14ec5831901b54d354a3d357
xmnlz <lemmeq9@gmail.com>
2026-06-15 23:24:16 +0200

    feat: trying git commit

--- home/tui/neovim/nvim/lua/plugins/gitsigns.lua
+++ home/tui/neovim/nvim/lua/plugins/gitsigns.lua
@@ -1,5 +1,43 @@
 local gitsigns = require('gitsigns')
 
+local function parse_remote(remote)
+  -- SSH format: git@github.com:owner/repo.git
+  local host, path = remote:match("git@([^:]+):(.+)")
+
+  -- HTTPS format: https://github.com/owner/repo.git
+  if not host then
+    host, path = remote:match("https?://([^/]+)/(.+)")
+  end
+
+  if not host or not path then
+    return nil, nil
+  end
+
+  path = path:gsub("%.git$", "")
+
+  return host, path
+end
+
+local function open_commit()
+  local blame = gitsigns.get_current_line_blame()
+  if not blame then return end
+
+  local sha = blame:match("^%x+")
+  if not sha then return end
+
+  local remote = vim.fn.system("git remote get-url origin"):gsub("\n", "")
+  local host, repo = parse_remote(remote)
+
+  if not host or not repo then
+    print("Could not parse git remote: " .. remote)
+    return
+  end
+
+  local url = "https://" .. host .. "/" .. repo .. "/commit/" .. sha
+
+  vim.fn.system({ "xdg-open", url })
+end
+
 gitsigns.setup {
   on_attach = function(bufnr)
     local function map(mode, lhs, rhs)
@@ -8,57 +46,6 @@ gitsigns.setup {
 
     map('n', '<leader>bb', gitsigns.blame)
     map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+    map('n', '<leader>go', open_commit)
   end,
 }
-
-local function detect_provider(remote)
-  if remote:match("github.com") then
-    return "github"
-  elseif remote:match("gitlab.com") then
-    return "gitlab"
-  elseif remote:match("bitbucket.org") then
-    return "bitbucket"
-  else
-    return "unknown"
-  end
-end
-
-local function extract_repo(remote)
-  return remote
-      :gsub("git@github.com:", "")
-      :gsub("git@gitlab.com:", "")
-      :gsub("git@bitbucket.org:", "")
-      :gsub("https://github.com/", "")
-      :gsub("https://gitlab.com/", "")
-      :gsub("https://bitbucket.org/", "")
-      :gsub("%.git$", "")
-end
-
-local function open_universal_change()
-  local blame = gitsigns.get_current_line_blame()
-  if not blame then return end
-
-  local sha = blame:match("^%x+")
-  if not sha then return end
-
-  local remote = vim.fn.system("git remote get-url origin"):gsub("\n", "")
-  local provider = detect_provider(remote)
-  local repo = extract_repo(remote)
-
-  local url
-
-  if provider == "github" then
-    url = "https://github.com/" .. repo .. "/commit/" .. sha
-  elseif provider == "gitlab" then
-    url = "https://gitlab.com/" .. repo .. "/-/commit/" .. sha
-  elseif provider == "bitbucket" then
-    url = "https://bitbucket.org/" .. repo .. "/commits/" .. sha
-  else
-    print("Unsupported git host: " .. remote)
-    return
-  end
-
-  vim.fn.system({ "xdg-open", url })
-end
-
-vim.keymap.set('n', '<leader>go', open_universal_change)
