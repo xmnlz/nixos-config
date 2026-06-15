3271e2c195e015fcd18f051aa683c918ae91b439
xmnlz <lemmeq9@gmail.com>
2026-06-15 23:37:06 +0200

    feat: final fix for gitsigns

--- home/tui/neovim/nvim/lua/plugins/gitsigns.lua
+++ home/tui/neovim/nvim/lua/plugins/gitsigns.lua
@@ -1,43 +1,5 @@
 local gitsigns = require('gitsigns')
 
-local function parse_remote(remote)
-  -- SSH format: git@github.com:owner/repo.git
-  local host, path = remote:match("git@([^:]+):(.+)")
-
-  -- HTTPS format: https://github.com/owner/repo.git
-  if not host then
-    host, path = remote:match("https?://([^/]+)/(.+)")
-  end
-
-  if not host or not path then
-    return nil, nil
-  end
-
-  path = path:gsub("%.git$", "")
-
-  return host, path
-end
-
-local function open_commit()
-  local blame = gitsigns.get_current_line_blame()
-  if not blame then return end
-
-  local sha = blame:match("^%x+")
-  if not sha then return end
-
-  local remote = vim.fn.system("git remote get-url origin"):gsub("\n", "")
-  local host, repo = parse_remote(remote)
-
-  if not host or not repo then
-    print("Could not parse git remote: " .. remote)
-    return
-  end
-
-  local url = "https://" .. host .. "/" .. repo .. "/commit/" .. sha
-
-  vim.fn.system({ "xdg-open", url })
-end
-
 gitsigns.setup {
   on_attach = function(bufnr)
     local function map(mode, lhs, rhs)
