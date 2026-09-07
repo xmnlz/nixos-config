80ccf29a67d5bda33f19daa27ed3819fbcfb6949
xmnlz <lemmeq9@gmail.com>
2026-09-07 14:42:46 +0200

    fix: upgrade to the new config format

--- modules/nixos/cleanup.nix
+++ modules/nixos/cleanup.nix
@@ -1,9 +1,9 @@
 {...}: {
-  services.journald.extraConfig = ''
-    SystemMaxUse=500M
-    SystemMaxFileSize=50M
-    MaxRetentionSec=1month
-  '';
+  services.journald.settings.Journal = {
+    SystemMaxUse = "500M";
+    SystemMaxFileSize = "50M";
+    MaxRetentionSec = "1month";
+  };
 
   systemd.coredump.settings.Coredump = {
     MaxUse = "2G";
