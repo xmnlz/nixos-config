edaf018e1367099dd6af66400a35fdc93eff9a5d
xmnlz <lemmeq9@gmail.com>
2026-09-22 00:06:13 +0200

    feat(zennotes): add zn-cli wrapper

--- home/gui/zennotes.nix
+++ home/gui/zennotes.nix
@@ -1,5 +1,12 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  zn-cli = pkgs.writeShellScriptBin "zn-cli" ''
+    exec ${pkgs.nodejs}/bin/node \
+      ${pkgs.zennotes-desktop}/lib/node_modules/zennotes-monorepo/apps/desktop/out/main/cli.js \
+      "$@"
+  '';
+in {
   home.packages = [
     pkgs.zennotes-desktop
+    zn-cli
   ];
 }
